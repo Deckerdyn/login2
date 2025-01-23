5fe53ee1cf9971af67a34f978e5703abdf05696c
Deckerdyn <pedrojhoel02@gmail.com>
2025-01-23 17:19:00 -0300

    Se cambió la duración del token de admin a ilimitado, del usuario normal a 12 horas y del usuario temporal a una hora

--- app/auth/roles.py
+++ app/auth/roles.py
@@ -1,12 +1,12 @@
 roles = {
     "admin": {
         "max_queries": None, #consultas ilimitadas 
-        "token_duration": 42 * 60 , #un día de duración 
+        "token_duration": None , #un día de duración 
         "access_schedule": None,
     },
     "usuario": {
-        "max_queries": 7,  # 3 consultas diarias
-        "token_duration": 2,  # 12 horas en minutos
+        "max_queries": 3,  # 3 consultas diarias
+        "token_duration": 12 * 60,  # 12 horas en minutos
         "access_schedule": {"start": 9, "end": 18},  # Acceso entre las 9:00 y 18:00
     },
     "temporal": {
